e9c5fe7ef005854a15836d7dba9f18a58310ae8c
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-03-18 11:25:14 +0800

    yes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using ModdingUtilities;
+using MelonLoader;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +36,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(ModMain), "ModdingUtilities", "v0.1", "MillimediaGames")]
+[assembly: MelonGame("Lychee Game Labs", "Patch Quest")]
